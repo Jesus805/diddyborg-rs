c4c7d6e92b2299bbb262980c4e0c0dd65a19735f
Jesus <jesus.bamford@gmail.com>
2020-11-25 00:06:19 -0800

    Added Command Enum

--- diddyborg/src/main.rs
+++ diddyborg/src/main.rs
@@ -1,3 +1,39 @@
+enum Command {
+    SET_LED,
+    GET_LED,
+    SET_A_FWD,
+    SET_A_REV,
+    GET_A,
+    SET_B_FWD,
+    SET_B_REV,
+    GET_B,
+    ALL_OFF,
+    RESET_EPO,
+    GET_EPO,
+    SET_EPO_IGNORE,
+    GET_EPO_IGNORE,
+    SET_ALL_FWD,
+    SET_ALL_REV,
+    SET_FAILSAFE,
+    GET_FAILSAFE,
+    SET_ENC_MODE,
+    GET_ENC_MODE,
+    MOVE_A_FWD,
+    MOVE_A_REV,
+    MOVE_B_FWD,
+    MOVE_ALL_FWD,
+    GET_ENC_MOVING,
+    SET_ENC_SPEED,
+    GET_ENC_SPEED,
+    GET_ID,
+    SET_I2C_ADD,
+    VALUE_FWD,
+    VALUE_REV,
+    VALUE_ON,
+    VALUE_OFF
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
